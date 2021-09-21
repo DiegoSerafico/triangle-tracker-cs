204dc407bf85b40928fa134b94a5f2c380f522f2
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-21 11:41:08 -0700

    add passed test for constructor

--- TriangleTracker.Tests/ModelsTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelsTests/TriangleTests.cs
@@ -7,6 +7,14 @@ namespace TriangleTracker.Tests
   public class TriangleTests
   {
     [TestMethod]
+    public void TriangleConstructorTest1() {
+      Triangle testTriangle = new Triangle(3, 3, 3);
+      Assert.AreEqual(3, testTriangle.Side1);
+      Assert.AreEqual(3, testTriangle.Side2);
+      Assert.AreEqual(3, testTriangle.Side3);
+    }
+
+    [TestMethod]
     public void CheckEquilateral_CheckAllLengthsEqual_True() {
       Triangle testTriangle = new Triangle(3, 3, 3);
       Assert.AreEqual(true, testTriangle.CheckEquilateral());
