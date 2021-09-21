2dcf0fdac6d576c4daef72b75497464f46781b68
Diego Serafico <dseraficohernandez@gmail.com>
2021-09-21 14:55:45 -0700

    add user error checks

--- TriangleTracker/Program.cs
+++ TriangleTracker/Program.cs
@@ -8,17 +8,24 @@ namespace TriangleTracker
     
     public static void Main(){
       
-      Console.WriteLine("Welcome to Triangle Tracker! Please enter a number.");
+      Console.WriteLine("Welcome to Triangle Tracker! Please enter a whole number.");
+      int side1;
+      int side2;
+      int side3;
       string stringSide1 = Console.ReadLine();
-      Console.WriteLine("Enter another number:");
+      Console.WriteLine("Enter another whole number:");
       string stringSide2 = Console.ReadLine();
-      Console.WriteLine("One more member, please:");
+      Console.WriteLine("One more whole number, please:");
       string stringSide3 = Console.ReadLine();
-      int side1 = int.Parse(stringSide1);
-      int side2 = int.Parse(stringSide2);
-      int side3 = int.Parse(stringSide3);
+      if (int.TryParse(stringSide1, out side1) && int.TryParse(stringSide2, out side2) && int.TryParse(stringSide3, out side3)) {
       Triangle usersTriangle = new Triangle(side1, side2, side3);
+      string result = usersTriangle.CheckType();
+      Console.WriteLine(result);
 
+      } else {
+        Console.WriteLine("You didn't enter only whole numbers. Try again.");
+        Main();
+      }
     }
   }
 }
